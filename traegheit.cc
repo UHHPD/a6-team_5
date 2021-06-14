b9de1dbf5a5fdbb1f8deb13fc0c66dee51125581
moritzhw <72267145+moritzhw@users.noreply.github.com>
2021-06-14 16:00:04 +0000

    Versuch 2

--- traegheit.cc
+++ traegheit.cc
@@ -6,7 +6,7 @@
 
 int main() {
   const int N = 10000;     // Anzahl Integrationspunkte
-  const double M = 1;      // Masse des Zylindermantels
+  const double M = 2;      // Masse des Zylindermantels
   const double ZM_R = 1.0; // Radius der Zylindermantels
   const double ZM_L = 1.0; // Laenge des Zylindermantels
 
@@ -18,6 +18,7 @@ int main() {
   std::cin >> u;
 
   std::unique_ptr<Zylindermantel> zm(new Zylindermantel(ZM_R, ZM_L));
+  std::unique_ptr<Vollzylinder> vz(new Vollzylinder(ZM_R, ZM_L));
 
   double J = 0;     // Massentraegheitsmoment
   double m = M / N; // Masse eines Massenpunktes
@@ -33,6 +34,18 @@ int main() {
   }
   std::cout << "Massentraegheitsmoment fuer einen Zylindermantel"
             << " mit a = " << a << " und u = " << u << ": " << J << std::endl;
-            
+  J=0;
+  for (int i = 0; i < N; ++i) {
+    Vektor x = vz->punkt();
+    // Abstand Punkt x und Gerade a + t*u
+    Vektor n = (x-a).kreuz(u);
+    // Vektor n = ...;//Normalenvektor x-a kreuz u
+    double r = n.betrag()/u.betrag(); //|n|/|u|
+    //std::cout << x << " :" << r << std::endl;
+    // addiere Beitrag des Massenpunktes zum Traegheitsmoment
+    J += m * r * r;
+  } 
+  std::cout << "Massentraegheitsmoment fuer einen Vollzylinder"
+            << " mit a = " << a << " und u = " << u << ": " << J << std::endl;
   return 0;
 }
